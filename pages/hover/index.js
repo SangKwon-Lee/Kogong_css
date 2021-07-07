5f32f8eefd813cb5f483026c59c7a3297f6a1d09
SangKwon-Lee <rnrel13@gmail.com>
2021-07-07 16:47:14 +0900

    first

--- pages/hover/index.js
+++ pages/hover/index.js
@@ -2,13 +2,11 @@ import { Img1, Wrapper, Img2, Img3, Img4 } from '../../src/hover';
 
 export default function HoverImg() {
 	return (
-		<>
-			<Wrapper>
-				<Img1>어둡다</Img1>
-				<Img2>우주는</Img2>
-				<Img3>마치</Img3>
-				<Img4>마음처럼</Img4>
-			</Wrapper>
-		</>
+		<Wrapper>
+			<Img1>어둡다</Img1>
+			<Img2>우주는</Img2>
+			<Img3>마치</Img3>
+			<Img4>마음처럼</Img4>
+		</Wrapper>
 	);
 }
--- pages/wave/index.js
+++ pages/wave/index.js
@@ -1,13 +1,11 @@
 import { Wrapper, Wave1, Wave2, Wave3, Wave4 } from '../../src/wave';
 export default function Wave() {
 	return (
-		<>
-			<Wrapper>
-				<Wave1></Wave1>
-				<Wave2></Wave2>
-				<Wave3></Wave3>
-				<Wave4></Wave4>
-			</Wrapper>
-		</>
+		<Wrapper>
+			<Wave1></Wave1>
+			<Wave2></Wave2>
+			<Wave3></Wave3>
+			<Wave4></Wave4>
+		</Wrapper>
 	);
 }
