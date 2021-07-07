7f5eba8ee51e7cbd922e7ea438b655e58f898fa8
SangKwon-Lee <rnrel13@gmail.com>
2021-07-07 16:42:32 +0900

    호버와 웨이브 애니메이션

---
+++ pages/wave/index.js
@@ -0,0 +1,13 @@
+import { Wrapper, Wave1, Wave2, Wave3, Wave4 } from '../../src/wave';
+export default function Wave() {
+	return (
+		<>
+			<Wrapper>
+				<Wave1></Wave1>
+				<Wave2></Wave2>
+				<Wave3></Wave3>
+				<Wave4></Wave4>
+			</Wrapper>
+		</>
+	);
+}
